3154170aa1984064cddd0a8c8c14cb78f19ee64f
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-06-29 00:51:53 +0530

    Time: 4 ms (49.95%), Space: 6 MB (47.31%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.cpp
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int sumOfMultiples(int n)
     {
-        int sum=0;
-        for(int i=1;i<=n;i++)
+        int sum=0,i=1;
+        for(;i<=n;i++)
         {
             if(i%3==0||i%5==0||i%7==0)
                 sum+=i;
