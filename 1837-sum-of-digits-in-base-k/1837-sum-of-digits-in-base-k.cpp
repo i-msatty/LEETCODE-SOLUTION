e276c20aaa4e76b0ca540b6522b199a3b937b129
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-07-17 01:28:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.52%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumBase(int n, int k) 
+    {
+        if(n==k)
+        {   
+            return 1;
+        } 
+    return sum(n,k);
+    }
+
+    int sum(int n,int k)
+    {
+        if(n<k)
+            return n%k;
+        else
+            return ((n%k)+sum(n/k,k));
+    }
+};
