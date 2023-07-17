d3dbce7dcb5972f2b0dc8ed87407e3cfb8d71096
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-07-17 14:46:32 +0530

    Time: 28 ms (93.47%), Space: 72.1 MB (29.87%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,58 +10,96 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<int> a1;
-        stack<int> a2;
-        stack<int> ans;
-        int carry = 0;
-        ListNode *temp1 = l1;
-        ListNode *temp2 = l2;
-        while(temp1 && temp2){
-            a1.push(temp1->val);
-            a2.push(temp2->val);
-            temp1=temp1->next;
-            temp2=temp2->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+        int carry=0;
+        ListNode* head1= NULL;
+        ListNode* head2= NULL;
+        ListNode* head3=NULL;
+        while(l1!=NULL)
+        {
+            ListNode* temp=new ListNode;
+            temp->val=l1->val;
+            temp->next=head1;
+            head1=temp;
+            l1=l1->next;
         }
-        while(temp1){
-            a1.push(temp1->val);
-            temp1=temp1->next;
+        while(l2!=NULL)
+        {
+            ListNode* temp=new ListNode;
+            temp->val=l2->val;
+            temp->next=head2;
+            head2=temp;
+            l2=l2->next;
         }
-        while(temp2){
-            a2.push(temp2->val);
-            temp2=temp2->next;
+        while(head1!=NULL &&  head2!=NULL)
+        {
+            ListNode* temp=new ListNode;
+            temp->val=head1->val+head2->val+carry;
+            {
+                if(temp->val>9)
+                {    temp->val=abs(10-temp->val);
+                     carry=1;
+                }
+                else
+                {
+                    carry=0;
+                }
+            }
+            temp->next=head3;
+            head3=temp;
+            head1=head1->next;
+            head2=head2->next;
         }
-        while(!a1.empty() && !a2.empty()){
-            int s = a1.top()+a2.top()+carry;
-            ans.push(s%10);
-            carry = s/10;
-            a1.pop();
-            a2.pop();
+        if(head1!=NULL && head2==NULL)
+        {
+            while(head1!=NULL)
+            {
+            ListNode* temp=new ListNode;
+            temp->val=head1->val;
+                if(carry!=0)
+                {
+                    temp->val+=carry;
+                    carry=0;
+                }
+                if(temp->val>9)
+                {
+                    carry=1;
+                    temp->val=abs(10-temp->val);
+                }
+            temp->next=head3;
+            head3=temp;
+            head1=head1->next;
+            }
         }
-        while(!a1.empty()){
-            int s = a1.top()+carry;
-            ans.push(s%10);
-            carry=s/10;
-            a1.pop();
+         if(head1==NULL && head2!=NULL)
+        {
+            while(head2!=NULL)
+            {
+            ListNode* temp=new ListNode;
+            temp->val=head2->val;
+                if(carry!=0)
+                {
+                    temp->val+=carry;
+                    carry=0;
+                }
+                if(temp->val>9)
+                {
+                    carry=1;
+                    temp->val=abs(10-temp->val);
+                }
+            temp->next=head3;
+            head3=temp;
+            head2=head2->next;
+            }
         }
-        while(!a2.empty()){
-            int s = a2.top()+carry;
-            ans.push(s%10);
-            carry=s/10;
-            a2.pop();
+        if(carry!=0)
+        {
+            ListNode* temp=new ListNode;
+            temp->val=carry;
+            temp->next=head3;
+            head3=temp;
         }
-        if(carry){
-            ans.push(1);
-        }
-        ListNode *f_ans = new ListNode(ans.top());
-        ans.pop();
-        ListNode *t = f_ans;
-        while(!ans.empty()){
-            ListNode *newnode = new ListNode(ans.top());
-            t->next = newnode;
-            t=newnode;
-            ans.pop();
-        }
-        return f_ans;
+     return head3;  
     }
 };
