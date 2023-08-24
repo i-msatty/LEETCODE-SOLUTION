4a981cae458a36db2f47aea8cf4c719507c59331
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-08-25 02:08:57 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (43.12%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int count;
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected;
+        for(int i=0;i<heights.size();i++)
+        {
+            expected.push_back(heights[i]);
+        }
+        sort(expected.begin(),expected.end());
+        for(int i=0;i<heights.size();i++)
+        {
+            if(heights[i]!=expected[i])
+                count++;
+        }
+        return count;
+    }
+};
