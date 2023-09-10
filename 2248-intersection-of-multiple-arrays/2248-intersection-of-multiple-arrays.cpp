7fe4a4e29f351fd1eaadf9ed68173bd3d623b079
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-09-10 20:11:07 +0530

    Time: 7 ms (88.43%), Space: 11.3 MB (99.38%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -4,20 +4,21 @@ public:
             vector<int> c;
             int total_no_array=nums.size();
             int arr[1001][2];
-            for(int i=0;i<1001;i++)
+            int i,j;
+            for( i=0;i<1001;i++)
             {
                     arr[i][0]=0;
             }
-        for(int i=0;i<nums.size();i++)
+        for(i=0;i<total_no_array;i++)
         {
-                for(int j=0;j<nums[i].size();j++)
+                for(j=0;j<nums[i].size();j++)
                 {
                         arr[nums[i][j]][0]++;  //count
                         arr[nums[i][j]][1]=nums[i][j]; //stores element
                 }
         }
 //cout<<"count "<<arr[3][0];
-         for(int i=0;i<1001;i++)
+         for(i=0;i<1001;i++)
          {
                  if(arr[i][0]==total_no_array)
                          c.push_back(arr[i][1]);
