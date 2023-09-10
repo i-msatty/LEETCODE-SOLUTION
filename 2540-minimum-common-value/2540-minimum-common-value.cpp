ad5b53321c38c51dc6f0006cdeeb780fc813c559
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-09-11 00:51:45 +0530

    Time: 157 ms (13.19%), Space: 66.8 MB (11.45%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s;
+            for(auto i:nums1)
+                    s.insert(i);
+            for(auto i:nums2)
+            {
+                    if(s.find(i)!=s.end())
+                            return i;
+            }
+            return -1;
+    }
+};
