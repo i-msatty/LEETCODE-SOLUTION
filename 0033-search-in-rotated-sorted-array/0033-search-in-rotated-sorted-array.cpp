a4a57da4c35927d70b3bc40fcd38710f91946ed6
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-09-10 13:48:12 +0530

    Time: 8 ms (20.75%), Space: 11.1 MB (20.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,38 +6,5 @@ public:
                 return index;
             else
                     return -1;}
-    //     int max= 10000;
-    //         int arr[max][2];
-    //         int i;
-    //         for(i=0;i<max;i++)
-    //         {
-    //                 arr[i][0]=-1;
-    //                 arr[i][1]=-1;
-    //         }
-    //         for(i=0;i<nums.size();i++)
-    //         {
-    //                 if(nums[i]>=0)
-    //                 {
-    //                 arr[nums[i]][0]= i;
-    //                 }
-    //                 else
-    //                 {
-    //                         arr[abs(nums[i])][1]=i;
-    //                 }
-    //         }
-    //         if(target>=0)
-    //         {
-    //                 if(arr[nums[target]][0]!=-1)
-    //                         return arr[nums[target]][0];
-    //                 else
-    //                         return -1;
-    //         }
-    //         else
-    //         {
-    //                  if(arr[nums[target]][1]!=-1)
-    //                         return arr[nums[target]][1];
-    //                 else
-    //                         return -1;
-    //         }
-    // }
+   
 };
