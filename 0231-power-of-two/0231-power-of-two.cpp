d83a7916ab212bdbebdb61996ae498d862d3cbc4
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-10-26 00:41:54 +0530

    Time: 5 ms (15.36%), Space: 6.5 MB (12.63%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n==0)
+            return false;
+        long long int  x= n;
+        x-=1;
+        return (n && !(n&(x)));
+    }
+};
