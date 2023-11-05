0fec7a807757855e51a2273b39f6dc73a417a2cd
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2023-11-05 10:54:21 +0530

    Time: 13 ms (24.57%), Space: 7.5 MB (81.70%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+#include<string>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<t.length();i++)
+        {
+            if(s[i]!=t[i])
+                return false;
+        }
+        return true;
+    }
+};
