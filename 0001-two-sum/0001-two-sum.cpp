40c1a0a768d0f6b7d5fa0a95c7280147887d2634
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2026-09-22 23:20:02 +0530

    Time: 5 ms (44.72%), Space: 14.8 MB (44.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-     vector<int> a;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=0;j<nums.size();j++)
-            {
-                if(i==j)
-                    continue;
-                if(nums[i]+nums[j]==target)
-                {
-                    a.push_back(i);
-                    a.push_back(j);
-                    return a;
-                }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for(int i=0;i<nums.size();i++){
+            int complement=target-nums[i];
+            auto it=hash.find(complement);
+            if(it!=hash.end()){
+                return {i,it->second};
+            }
+            else{
+                hash.insert({nums[i],i});
             }
         }
-        return a;
+        return {};
     }
 };
-
-/*int* twoSum(int* nums, int numsSize, int target, int* returnSize)
-{
-    int i,j;
-    returnSize=(int*)malloc(sizeof(int)*2);
-    for(i=0;i<numsSize;i++)
-    { 
-        for(j=0;j<numsSize;j++)
-        {
-            if(i==j)
-                continue;
-            if(nums[i]+nums[j]==target)
-            {
-               returnSize[0]=i;
-               returnSize[1]=j;
-            }
-        }
-    }
-return returnSize;
-}*/
